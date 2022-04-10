68c69bc832a0df3606577ce5d50118c89bfc3391
comp3335gp22 <foktain123a@outlook.com>
2022-04-10 17:19:18 +0800

    bug fix: add a if statement to check the err

--- routes/auth.js
+++ routes/auth.js
@@ -12,66 +12,68 @@ router.use(cookieParser());
 
 // Login page
 router
-  .route("/")
-  .get((req, res) => {
-    // render the login page
-    res.render("login", {});
-  })
-  .post((req, res) => {
-    // retrieve information from request
-    const email = req.body.email;
-    const password = req.body.password;
+    .route("/")
+    .get((req, res) => {
+        // render the login page
+        res.render("login", {});
+    })
+    .post((req, res) => {
+        // retrieve information from request
+        const email = req.body.email;
+        const password = req.body.password;
 
-    const user = new User({
-      email: email,
-      password: password,
-    });
-    // login the user
-    req.login(user, (err) => {
-      if (err) {
-        console.log(err);
-        console.log("Cannot log in");
-        const errorHeading = "Invalid user credentials";
-        const errorText = "Please try to login again";
-        const errorBtnText = "Head back to login";
-        const redirectLink = "/";
-        res.render("error", {
-          errorHeading: errorHeading,
-          errorText: errorText,
-          errorBtnText: errorBtnText,
-          redirectLink: redirectLink,
+        const user = new User({
+            email: email,
+            password: password,
         });
-      } else {
-        passport.authenticate("local")(req, res, (err) => {
-          const errorHeading = "Invalid user credentials";
-          const errorText = "Please try to login again";
-          const errorBtnText = "Head back to login";
-          const redirectLink = "/";
-          res.render("error", {
-            errorHeading: errorHeading,
-            errorText: errorText,
-            errorBtnText: errorBtnText,
-            redirectLink: redirectLink,
-          });
+        // login the user
+        req.login(user, (err) => {
+            if (err) {
+                console.log(err);
+                console.log("Cannot log in");
+                const errorHeading = "Invalid user credentials";
+                const errorText = "Please try to login again";
+                const errorBtnText = "Head back to login";
+                const redirectLink = "/";
+                res.render("error", {
+                    errorHeading: errorHeading,
+                    errorText: errorText,
+                    errorBtnText: errorBtnText,
+                    redirectLink: redirectLink,
+                });
+            } else {
+                passport.authenticate("local")(req, res, (err) => {
+                    const errorHeading = "Invalid user credentials";
+                    const errorText = "Please try to login again";
+                    const errorBtnText = "Head back to login";
+                    const redirectLink = "/";
+                    if (err) {
+                        res.render("error", {
+                            errorHeading: errorHeading,
+                            errorText: errorText,
+                            errorBtnText: errorBtnText,
+                            redirectLink: redirectLink,
+                        });
+                    }
 
-          console.log("Logged in successful");
-          // setup encrypted cookie
-          const cipherID = CryptoJS.AES.encrypt(
-            String(user._id),
-            process.env.COOKIE_KEY
-          ).toString();
-          console.log("[Encrypted ID] " + cipherID);
+                    console.log("Logged in successful");
+                    // setup encrypted cookie
+                    const cipherID = CryptoJS.AES.encrypt(
+                        String(user._id),
+                        process.env.COOKIE_KEY
+                    ).toString();
+                    console.log("[Encrypted ID] " + cipherID);
 
-          res.cookie("user", cipherID, {
-            expires: new Date(Date.now() + 3600000),
-            secure: true,
-            httpOnly: true,
-          });
-          // render the main page
-          res.redirect("/main");
+                    res.cookie("user", cipherID, {
+                        expires: new Date(Date.now() + 3600000),
+                        secure: true,
+                        httpOnly: true,
+                    });
+                    // render the main page
+                    res.redirect("/main");
+                });
+            }
         });
-      }
     });
-  });
 
 module.exports = router;
